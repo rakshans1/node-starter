37303c70ec3da2244f0fdaa12837ba861b7caf58
Rakshan Shetty <shetty.raxx555@gmail.com>
2024-05-15 15:25:21 +0530

    feat: add flake

--- README.md
+++ README.md
@@ -1,1 +1,30 @@
-# node-starter
+# Node Starter
+
+## 🏗️ Prerequisites
+
+- [nix](https://nix.dev/tutorials/install-nix)
+- [direnv](https://direnv.net/docs/installation.html)
+
+Setup direnv with [shell](https://direnv.net/docs/hook.html)
+
+Run `direnv allow`
+
+```
+Node: 20.12.2
+Yarn: 1.22.22
+```
+
+`nix flake show https://github.com/rakshans1/node-starter`
+
+```
+github:rakshans1/node-starter/main
+└───devShells
+    ├───aarch64-darwin
+    │   └───default: development environment 'nix-shell'
+    ├───aarch64-linux
+    │   └───default: development environment 'nix-shell'
+    ├───x86_64-darwin
+    │   └───default: development environment 'nix-shell'
+    └───x86_64-linux
+        └───default: development environment 'nix-shell'
+```
---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  description = "Node Starter";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+
+        inherit (pkgs) inotify-tools terminal-notifier;
+        inherit (pkgs.lib) optionals;
+        inherit (pkgs.stdenv) isDarwin isLinux;
+
+        linuxDeps = optionals isLinux [ inotify-tools ];
+        darwinDeps = optionals isDarwin [ terminal-notifier ]
+          ++ (with pkgs.darwin.apple_sdk.frameworks; optionals isDarwin [
+          CoreFoundation
+          CoreServices
+        ]);
+
+        nodejs = pkgs.nodejs_20;
+        yarn = pkgs.yarn.override { nodejs = nodejs; };
+
+      in
+      {
+        devShells = {
+          default = pkgs.mkShell {
+            packages = with pkgs;  [
+               nodejs yarn
+            ] ++ linuxDeps ++ darwinDeps;
+            shellHook = ''
+              export PATH=bun:$PATH
+            '';
+          };
+        };
+      });
+}
